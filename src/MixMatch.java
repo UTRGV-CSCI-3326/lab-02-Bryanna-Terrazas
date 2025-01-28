e3ecbc55237b5ef7763e9b3d6dbbf1f30f98e7ba
Bryanna-Terrazas <bryanna.terrazas01@utrgv.edu>
2025-01-28 21:33:59 +0000

    First commit (will add facts soon)

---
+++ src/MixMatch.java
@@ -0,0 +1,23 @@
+public class MixMatch{
+    public static void main(String[] args){
+        char letter = 'A';
+        System.out.println(letter);
+        String word = "Banana";
+        System.out.println(word);
+        short num = 1;
+        System.out.println(num);
+        int numb = 23;
+        System.out.println(numb);
+        long number = 45678910;
+        System.out.println(number);
+        float p = 3;
+        System.out.println(p);
+        double point = 2.30404050606;
+        System.out.println(point);
+        boolean isTrue = true;
+        System.out.println(isTrue);
+
+
+        
+    }
+}
